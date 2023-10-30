3023778359f44f5635f7784831ef8e580396f49e
Bishwajit Paul <paul.bishwajit09@gmail.com>
2023-10-30 18:38:54 +0600

    Committed

--- src/components/customerComments/CustomerComments.jsx
+++ src/components/customerComments/CustomerComments.jsx
@@ -1,7 +1,10 @@
 import { FaQuoteRight, FaStar } from "react-icons/fa";
 
 import ClientLogo from "../../assets/icons/person.svg";
-import Quotation from "../../assets/icons/quote.svg";
+import CoolBgImageFour from "../../assets/testimonial-bgs/cool-background4.png";
+import CoolBgImageOne from "../../assets/testimonial-bgs/cool-background1.png";
+import CoolBgImageThree from "../../assets/testimonial-bgs/cool-background3.png";
+import CoolBgImageTwo from "../../assets/testimonial-bgs/cool-background2.png";
 import SectionTitleCenter from "../common/sectionTitleCenter/SectionTitleCenter";
 
 const CustomerComments = () => {
@@ -14,12 +17,17 @@ const CustomerComments = () => {
           "The majority have suffered alteration in some form, by injected humour, or randomized words which don't look even slightly believable."
         }
       />
-      <div className="lg:-mt-24 lg:pb-5">
-        <div className="carousel w-full">
-          <div id="slide1" className="carousel-item relative w-full h-96">
-            <div className="absolute transform translate-y top-1/3 ">
+      <div className="lg:mt-20">
+        <div className="carousel w-full lg:text-white lg:shadow-lg">
+          <div id="slide5" className="carousel-item relative w-full">
+            <img
+              src={CoolBgImageOne}
+              className="w-full rounded-md hidden lg:block"
+            />
+            <div className="absolute top-0 left-0 right-0 bottom-0 lg:bg-gradient-to-r from-sky-400 from-10% via-sky-500 via-30% to-emerald-500 to-90% rounded-lg opacity-30"></div>
+            <div className="absolute transform -translate-y lg:top-1/4">
               <div className="grid lg:grid-cols-12 gap-6 justify-between">
-                <div className="col-span-6 border space-y-6 rounded-md p-10">
+                <div className="col-span-6 shadow-md lg:space-y-6 rounded-md p-2 lg:p-10 lg:ml-10">
                   <div className="flex justify-between items-center space-x-2">
                     <div className="flex items-center space-x-2">
                       <div className="">
@@ -48,7 +56,7 @@ const CustomerComments = () => {
                     <FaStar />
                   </div>
                 </div>
-                <div className="col-span-6 border space-y-6 p-10 rounded-md">
+                <div className="col-span-6 shadow-md lg:space-y-6 rounded-md p-2 lg:p-10 lg:mr-10">
                   <div className="flex justify-between items-center space-x-2">
                     <div className="flex items-center space-x-2">
                       <div className="">
@@ -80,18 +88,26 @@ const CustomerComments = () => {
               </div>
             </div>
             <div className="absolute flex justify-center transform -translate-y left-5 right-5  bottom-2">
-              <a href="#slide4" className="btn btn-circle">
+              <a
+                href="#slide8"
+                className="btn btn-circle bg-gradient-to-r from-cyan-500 to-blue-500 text-white border-none"
+              >
                 ❮
               </a>
-              <a href="#slide2" className="btn btn-circle">
+              <a
+                href="#slide6"
+                className="btn btn-circle bg-gradient-to-r from-cyan-500 to-blue-500 text-white border-none"
+              >
                 ❯
               </a>
             </div>
           </div>
-          <div id="slide2" className="carousel-item relative w-full">
-            <div className="absolute transform translate-y top-1/3 ">
+          <div id="slide6" className="carousel-item relative w-full">
+            <img src={CoolBgImageTwo} className="w-full rounded-md" />
+            <div className="absolute top-0 left-0 right-0 bottom-0 lg:bg-gradient-to-r from-sky-400 from-10% via-sky-500 via-30% to-emerald-500 to-90% rounded-lg opacity-30"></div>
+            <div className="absolute transform -translate-y top-1/4">
               <div className="grid lg:grid-cols-12 gap-6 justify-between">
-                <div className="col-span-6 border space-y-6 p-10 rounded-md">
+                <div className="col-span-6 shadow-md lg:space-y-6 rounded-md p-2 lg:p-10 lg:ml-10">
                   <div className="flex justify-between items-center space-x-2">
                     <div className="flex items-center space-x-2">
                       <div className="">
@@ -120,7 +136,7 @@ const CustomerComments = () => {
                     <FaStar />
                   </div>
                 </div>
-                <div className="col-span-6 border space-y-6 p-10 rounded-md">
+                <div className="col-span-6 shadow-md lg:space-y-6 rounded-md p-2 lg:p-10 lg:mr-10">
                   <div className="flex justify-between items-center space-x-2">
                     <div className="flex items-center space-x-2">
                       <div className="">
@@ -151,19 +167,27 @@ const CustomerComments = () => {
                 </div>
               </div>
             </div>
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide1" className="btn btn-circle">
+            <div className="absolute flex justify-center transform -translate-y left-5 right-5  bottom-2">
+              <a
+                href="#slide5"
+                className="btn btn-circle bg-gradient-to-r from-cyan-500 to-blue-500 text-white border-none"
+              >
                 ❮
               </a>
-              <a href="#slide3" className="btn btn-circle">
+              <a
+                href="#slide7"
+                className="btn btn-circle bg-gradient-to-r from-cyan-500 to-blue-500 text-white border-none"
+              >
                 ❯
               </a>
             </div>
           </div>
-          <div id="slide3" className="carousel-item relative w-full">
-            <div className="absolute transform translate-y top-1/3 ">
+          <div id="slide7" className="carousel-item relative w-full">
+            <img src={CoolBgImageThree} className="w-full rounded-md" />
+            <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-sky-400 from-10% via-sky-500 via-30% to-emerald-500 to-90% rounded-lg opacity-30"></div>
+            <div className="absolute transform -translate-y top-1/4">
               <div className="grid lg:grid-cols-12 gap-6 justify-between">
-                <div className="col-span-6 border space-y-6 p-10 rounded-md">
+                <div className="col-span-6 shadow-md lg:space-y-6 rounded-md p-2 lg:p-10 lg:ml-10">
                   <div className="flex justify-between items-center space-x-2">
                     <div className="flex items-center space-x-2">
                       <div className="">
@@ -192,7 +216,7 @@ const CustomerComments = () => {
                     <FaStar />
                   </div>
                 </div>
-                <div className="col-span-6 border space-y-6 p-10 rounded-md">
+                <div className="col-span-6 border-gray-500 shadow-md lg:space-y-6 p-10 rounded-md lg:mr-10">
                   <div className="flex justify-between items-center space-x-2">
                     <div className="flex items-center space-x-2">
                       <div className="">
@@ -223,19 +247,27 @@ const CustomerComments = () => {
                 </div>
               </div>
             </div>
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide2" className="btn btn-circle">
+            <div className="absolute flex justify-center transform -translate-y left-5 right-5  bottom-2">
+              <a
+                href="#slide6"
+                className="btn btn-circle bg-gradient-to-r from-cyan-500 to-blue-500 text-white border-none"
+              >
                 ❮
               </a>
-              <a href="#slide4" className="btn btn-circle">
+              <a
+                href="#slide8"
+                className="btn btn-circle bg-gradient-to-r from-cyan-500 to-blue-500 text-white border-none"
+              >
                 ❯
               </a>
             </div>
           </div>
-          <div id="slide4" className="carousel-item relative w-full">
-            <div className="absolute transform translate-y top-1/3 ">
+          <div id="slide8" className="carousel-item relative w-full">
+            <img src={CoolBgImageFour} className="w-full rounded-md" />
+            <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-r from-sky-400 from-10% via-sky-500 via-30% to-emerald-500 to-90% rounded-lg opacity-30"></div>
+            <div className="absolute transform -translate-y top-1/4">
               <div className="grid lg:grid-cols-12 gap-6 justify-between">
-                <div className="col-span-6 border space-y-6 p-10 rounded-md">
+                <div className="col-span-6 shadow-md lg:space-y-6 p-10 rounded-md lg:ml-10">
                   <div className="flex justify-between items-center space-x-2">
                     <div className="flex items-center space-x-2">
                       <div className="">
@@ -264,7 +296,7 @@ const CustomerComments = () => {
                     <FaStar />
                   </div>
                 </div>
-                <div className="col-span-6 border space-y-6 p-10 rounded-md">
+                <div className="col-span-6 shadow-md lg:space-y-6 p-10 rounded-md lg:mr-10">
                   <div className="flex justify-between items-center space-x-2">
                     <div className="flex items-center space-x-2">
                       <div className="">
@@ -295,11 +327,17 @@ const CustomerComments = () => {
                 </div>
               </div>
             </div>
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide3" className="btn btn-circle">
+            <div className="absolute flex justify-center transform -translate-y left-5 right-5  bottom-2">
+              <a
+                href="#slide7"
+                className="btn btn-circle bg-gradient-to-r from-cyan-500 to-blue-500 text-white border-none"
+              >
                 ❮
               </a>
-              <a href="#slide1" className="btn btn-circle">
+              <a
+                href="#slide5"
+                className="btn btn-circle bg-gradient-to-r from-cyan-500 to-blue-500 text-white border-none"
+              >
                 ❯
               </a>
             </div>
